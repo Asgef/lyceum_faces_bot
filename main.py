9497d0c64f0d71d7f0c2f6a529057657c3f19a46
Asgef <asgefes1@gmail.com>
2024-11-25 15:32:54 +0300

    Add image in message

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
-from multiprocessing.util import DEBUG
-
-import telebot
+from telebot import TeleBot, types
 import logging
 import pprint
+from pathlib import Path
+import os
 
 
 # Настройки проекта
 TOKEN = ''
 DEBUG = True  # Включить логирование
 
+# Определяем пути до базовой директории проекта и директории с медиафайлами
+BASE_DIR = Path(__file__).resolve().parent
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_files')
+
 
 # Создаем объект бота и передаём ему токен регистрации в телеграмм.
-bot = telebot.TeleBot(TOKEN)
+bot = TeleBot(TOKEN)
 
 
 # Настройки логирования. Для отладки и исследования.
@@ -30,13 +34,34 @@ def start(message):
     # Исследуем объект message.
     logging.info(pprint.pformat(message.from_user.__dict__))
 
-    # Сохраняем имя пользователя в переменную и отвечаем ему.
+    # Сохраняем имя пользователя в переменную.
     user_name = message.from_user.first_name
-    bot.send_message(
-        message.chat.id,
+
+    # Подготавливаем сообщение
+    # текст
+    message_text = (
         f'Привет, {user_name}! \n'
         f'Здесь ты узнаешь истории выпускников лицея 1580'
-    )
+    ),
+    # Картинка
+    photo_path = os.path.join(MEDIA_ROOT, 'shcool_1580_logo.png')
+    with open(photo_path, 'rb') as photo:
+
+        # Подготавливаем копки
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton("О проекте")
+        item2 = types.KeyboardButton("Наши выпускники")
+        markup.add(item1, item2)
+
+        # Отправляем фото с текстом и кнопками
+        bot.send_photo(
+            message.chat.id,
+            photo=photo,
+            caption=message_text,
+            reply_markup=markup
+        )
+
+# @bot.message_handler()
 
 
 # Вызываем бота. Он запускается и ждёт команду.
